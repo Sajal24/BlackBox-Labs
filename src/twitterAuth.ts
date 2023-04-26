1e8bf8f1cd76544127ce3c05765399eaedc46151
Sajal Sharma <sajalsharma2408@gmail.com>
2023-04-26 20:10:40 +0530

    done

--- src/twitterAuth.ts
+++ src/twitterAuth.ts
@@ -1,84 +1,94 @@
-//firebase setup
-import { TwitterApi } from "twitter-api-v2";
-import dotenv from "dotenv";
-import express from "express";
+const express = require("express");
+const passport = require("passport");
+const { Strategy } = require("@superfaceai/passport-twitter-oauth2");
+const session = require("express-session");
+require("dotenv").config();
 
-dotenv.config();
-const appE = express();
-
-const stateDB = {
-    state: "",
-    codeVerifier: "",
-    accessToken: "",
-    refreshToken: "",
-};
-
-//twitter setup
-const client = new TwitterApi({
-    clientId: `${process.env.CLIENT_ID}`,
-    clientSecret: `${process.env.CLIENT_SECRET}`,
+// <1> Serialization and deserialization
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
 });
 
-const CALLBACK_URL = "http://127.0.0.1:5500/index.html";
-const port = 3000;
-
-const { url, codeVerifier, state } = client.generateOAuth2AuthLink(
-    CALLBACK_URL,
-    { scope: ["tweet.read", "users.read", "offline.access", "tweet.write"] }
-);
-// storing the code verifier and the state in the database
-stateDB.state = state;
-stateDB.codeVerifier = codeVerifier;
-
-appE.get("/", (req: string, res: any) => {
-    res.send("Hello World!");
-});
-
-appE.get(CALLBACK_URL, async (req: string, res: any) => {
-    const code = res.query.code;
-    const state = res.query.state;
-
-    let codeVerifier = stateDB.codeVerifier;
-    let stateFromDb = stateDB.state;
-
-    if (state !== stateFromDb) {
-        return res.status(400).send("Invalid state");
+// Use the Twitter OAuth2 strategy within Passport
+passport.use(
+  // <2> Strategy initialization
+  new Strategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      clientType: "confidential",
+      callbackURL: `${process.env.BASE_URL}/auth/twitter/callback`,
+    },
+    // <3> Verify callback
+    (accessToken, refreshToken, profile, done) => {
+      console.log("Success!", { accessToken, refreshToken });
+      return done(null, profile);
     }
+  )
+);
 
-    // exchange the code for an access token
-    client
-        .loginWithOAuth2({ code, codeVerifier, redirectUri: CALLBACK_URL })
-        .then(async ({ client: loggedClient, accessToken, refreshToken }) => {
-            // store the access token and refresh token in the database
-            // await settingDoc(twitterDocRef, { accessToken, refreshToken });
+const app = express();
 
-            stateDB.accessToken = accessToken;
-            stateDB.refreshToken = refreshToken || "";
+// <4> Passport and session middleware initialization
+app.use(passport.initialize());
+app.use(
+  session({ secret: "keyboard cat", resave: false, saveUninitialized: true })
+);
 
-            // use the logged client to make requests
-            const user = await loggedClient.v2.user("me");
-            console.log(user);
-            res.send(user);
-        })
-        .catch(console.error);
+app.get("/", (req, res) => {
+  res.end(`<h1>Authentication example</h1>`);
 });
 
-//refresh token setup
+// <5> Start authentication flow
+app.get(
+  "/auth/twitter",
+  passport.authenticate("twitter", {
+    // <6> Scopes
+    scope: ["tweet.read", "users.read", "offline.access", "tweet.write"],
+  })
+);
+
+// <7> Callback handler
+app.get(
+  "/auth/twitter/callback",
+  passport.authenticate("twitter"),
+  function (req, res) {
+    const userData = JSON.stringify(req.user, undefined, 2);
+    res.end(
+      `<h1>Authentication succeeded</h1> User data: <pre>${userData}</pre>`
+    );
+  }
+);
+
+//for the refesh token and tweeting and replying
+const client = new TwitterApi({
+  clientId: `${process.env.CLIENT_ID}`,
+  clientSecret: `${process.env.CLIENT_SECRET}`,
+});
+
 const refresh = async () => {
-    let refreshToken = stateDB.refreshToken;
+  //TODO: get the refresh token from above
 
-    const {
-        client: refreshedClient,
-        accessToken,
-        refreshToken: newRefreshToken,
-    } = await client.refreshOAuth2Token(refreshToken);
+  const {
+    client: refreshedClient,
+    accessToken,
+    refreshToken: newRefreshToken,
+  } = await client.refreshOAuth2Token(refreshToken);
 
-    // store the new access token and refresh token in the database
-    stateDB.accessToken = accessToken;
-    stateDB.refreshToken = newRefreshToken || "";
+  // store the new access token and refresh token in the database
+  //TODO: store the new access token and refresh token
+
+  //tweet function
 };
 
-appE.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+//tweet function
+const tweet = async (tweetMsg) => {
+  await refreshedClient.v2.tweet(tweetMsg);
+};
+
+app.listen(3030, "127.0.0.1", () => {
+  console.log(`Listening on ${process.env.BASE_URL}`);
 });
-//openai setup
